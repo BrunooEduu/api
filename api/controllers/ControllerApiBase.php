e5c40bedf5fea6cba68173dab6728f7a96cf1e7e
gelvazio <gelvazio@tidas.com.br>
2022-08-13 13:30:30 -0300

    ok

--- api/controllers/ControllerApiBase.php
+++ api/controllers/ControllerApiBase.php
@@ -21,18 +21,6 @@ class ControllerApiBase {
     }
 
     public function test(Request $request, Response $response, array $args) {
-    
-        $data = array(
-            "data" => date("Y-m-d H:i:s"),
-            "body"=>$request->getParsedBody(),
-            "headers"=>$request->getHeaders()
-        );
-        
-        return $response->withJson($data, 200);
-    }
-    
-    public function test02(Request $request, Response $response, array $args) {
-    
         $data = array(
             "data" => date("Y-m-d H:i:s"),
             "body"=>$request->getParsedBody(),
