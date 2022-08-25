ee5e3e1fd0f2e3f21a688cd8e6830834029016a3
gelvazio <gelvazio@tidas.com.br>
2022-08-25 12:13:43 -0300

    ajustes

---
+++ api/build-gelvazio.bat
@@ -0,0 +1,7 @@
+#Limpa-as-imagens-anteriores
+@echo off
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm -f %%i
+FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi -f %%i
+
+#Faz-build-aplicacao...
+docker build . -t api
--- api/run.bat
+++ api/run.bat
@@ -1,4 +1,5 @@
 docker run -p 3333:80 -it ^
     --rm ^
     -v %cd%:/var/www/html ^
+	--name apiphp ^
     api
